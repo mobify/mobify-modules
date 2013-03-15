36f18b1bdb5f7f2d3d88e6252d20527855c5c1c2
Noah Adams <noah.adams@gmail.com>
2013-03-15 15:59:41 -0700

    gruntify zoomable

---
+++ zoomable/Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        localConfig: (function(){ 
+                        try { 
+                            return grunt.file.readJSON('localConfig.json') 
+                        } catch(e) {
+                            return {};
+                        }
+                    })(),
+        releaseName: '<%= pkg.name %>-<%= pkg.version %>',
+        releaseMessage: '<%= pkg.name %> release <%= pkg.version %>',
+        clean: {
+            buildProducts: "build/"
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/zoomable.js',
+                dest: 'build/zoomable.min.js'
+            }
+        },
+        shell: {
+            tagRelease: {
+                command: 'git tag -a <%= releaseName %> -m "<%= releaseMessage %>" &&' +
+                  'git push origin <%= releaseName %>'
+            }
+        },
+        zip: {
+            "build/zoomable.zip": ["src/zoomable.js"]
+        },
+        s3: {
+            key: '<%= localConfig.aws.key %>',
+            secret: '<%= localConfig.aws.secret %>',
+            bucket: '<%= localConfig.aws.bucket %>',
+            access: "public-read",
+            headers: { "Cache-Control": "max-age=1200" },
+            upload: [
+                { // build
+                    src: "build/*",
+                    dest: "modules-testing/zoomable/<%= pkg.version %>/",
+                    rel: "build"
+                }
+            ]
+        }
+        // TODO: upload over a LATEST version and/or create a redirect?
+    });
+
+    // Load the task plugins
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-s3');
+    grunt.loadNpmTasks('grunt-clean');
+
+    // Default task(s).
+    grunt.registerTask('build', ['uglify', 'zip']);
+    grunt.registerTask('release', ['build', 'shell:tagRelease', 's3']);
+    grunt.registerTask('default', 'build');
+
+};
